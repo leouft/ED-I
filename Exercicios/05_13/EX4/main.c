62628fe0beed3877360514d722749f01be8f2f3b
leouft <leonardo.soares1@mail.uft.edu.br>
2026-05-27 00:25:57 -0300

    mais exercicio

---
+++ Exercicios/05_13/EX4/main.c
@@ -0,0 +1,14 @@
+#include "TLList.h"
+#include <stdio.h>
+
+int main(){
+    TLList* lista1 = TLList_create();
+    int V[] = {1, 2, 3, 4, 5}, i;
+    for(i = 0; i<5; i++)
+        if(!TLList_insert(lista1, V[i]))
+            printf("Ocorreu um erro ao inserir o V[%i]=%i\n", i, V[i]);
+    TLList_print(lista1);
+    TLList* lista_pares = TLList_pares(lista1);
+    TLList_print(lista_pares);
+    return 0;
+}
---
+++ Exercicios/05_13/EX5/TLList.h
@@ -0,0 +1,23 @@
+#ifndef TLLIST_H
+#define TLLIST_H
+#include <stdbool.h>
+
+typedef struct _list TLList;
+/**
+ * Cria instâncias da TLList;
+ */
+TLList* TLList_create();
+/**
+ * Política de inserção no fim da lista;
+ * @return true|false caso não possa inserir na lista
+ */
+bool TLList_insert(TLList*, int);
+
+/**
+ * Imprime a lista do início para o fim.
+ */
+void TLList_print(TLList*);
+
+TLList* TLList_remove_repeated(TLList*);
+
+#endif
